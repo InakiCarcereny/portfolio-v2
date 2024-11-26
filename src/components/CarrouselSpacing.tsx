ab285de13112649d3aaf5a9248793de399c173dd
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-26 18:30:12 -0300

    Add CarouselSpacing component to improve skills display and refactor Skills section to utilize new carousel layout for better UI

---
+++ src/components/CarrouselSpacing.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+import { skillsCards } from "@/lib/SkillsCard";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function CarouselSpacing() {
+  return (
+    <Carousel className="w-full max-w-full">
+      <CarouselContent className="-ml-4">
+        {skillsCards.map(({ title, icon: Icon }) => (
+          <CarouselItem key={title} className="basis-1/5">
+            <div className="flex items-center justify-center gap-2 bg-zinc-100 rounded-[4px] p-4 h-[200px]">
+              {/* <Icon /> */}
+              <p className="text-sm font-semibold">{title}</p>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
