25ba4e504ec1f2058b25b45df14f7ef3f2d1f559
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-26 17:59:25 -0300

    .

--- src/lib/NavItems.ts
+++ src/lib/NavItems.ts
@@ -1,18 +1,14 @@
 export const navItems = [
   {
     label: "About",
-    href: "/",
   },
   {
     label: "Projects",
-    href: "/projects",
   },
   {
     label: "Skills",
-    href: "/skills",
   },
   {
     label: "Contact",
-    href: "/contact",
   },
 ] as const;
